1dfe85877f8d12a10ed12eee6a4d7b9d32115f25
templace <binhnhivo2010@gmail.com>
2022-04-27 15:49:07 +0700

    hihi

--- main.js
+++ main.js
@@ -25,43 +25,13 @@
         
     
         // drop
-    var accordionHeadingEls = document.querySelectorAll(".accordion__item-minus .accordion__item-plus");
-
-    function accordion() {
-        for (let item of accordionHeadingEls) {
-            let accordionIcon = item.lastElementChild;
-            item.addEventListener("click", function() {
-                let accordionDetails = this.nextElementSibling;
-
-                for (let item2 of accordionHeadingEls) {
-                    if (item2 != item) {
-                        item2.nextElementSibling.style.maxHeight = null;
-                        item2.lastElementChild.classList.remove("accordion__icon--turn");
-                        item2.classList.remove('accordion__heading--active');
-
-                        // item2.classList.remove('show--active');
-                    }
-                }
-                item.classList.toggle("accordion__heading--active");
-
-                accordionIcon.classList.toggle("accordion__icon--turn");
-                if (accordionDetails.style.maxHeight != 0) {
-                    accordionDetails.style.maxHeight = null;
-                } else {
-                    accordionDetails.style.maxHeight = accordionDetails.scrollHeight + "px";
-                }
-              
-            });
-        }
+        
+        // Enroll Now
+    function Enroll(){
+        alert("Enroll Now");
     }
-    accordion();
-
-                // Enroll Now
-            function Enroll(){
-                alert("Enroll Now");
-            }
-            // header__close
-            document.querySelector('#megax').addEventListener("click", function() {
-                document.querySelector('.header__close').style.display = "none";
-            });
+        // header__close
+    document.querySelector('#megax').addEventListener("click", function() {
+        document.querySelector('.header__close').style.display = "none";
+    });
 
